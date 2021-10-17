ba3e934da6eff65beb91c56fa873ec2351bebf43
deepasriseenivasan <deepasrideeps1997@gmail.com>
2021-10-17 13:52:27 +0530

    5th commit

---
+++ Ex7/ex7.rb
@@ -0,0 +1,15 @@
+puts "Mary had a little lamb."
+puts "Its fleece was white as #{'snow'}."
+puts "*" * 10  # what'd that do?
+
+end1 = "D"
+end2 = "e"
+end3 = "e"
+end4 = "p"
+end5 = "a"
+end6 = "s"
+end7 = "r"
+end8 = "i"
+print end1 + end2 + end3 + end4 + end5 
+puts end6 + end7 + end8
+
