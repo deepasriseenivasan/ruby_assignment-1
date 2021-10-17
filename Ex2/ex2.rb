cd64de6f121c23e5fd182f2d883d9f192f16825c
deepasriseenivasan <deepasrideeps1997@gmail.com>
2021-10-17 12:44:37 +0530

    3nd commit

---
+++ Ex2/ex2.rb
@@ -0,0 +1,4 @@
+#puts "Hello World!"
+#puts "Hello Again"
+puts "Im Deepa"
+#puts "I am from coimbatore"
--- ex15.rb
+++
@@ -1,8 +0,0 @@
-file_1, file_2 = ARGV
-txt= open(file_1)
-asd= txt.read
-var=open(file_2, 'w')
-var.truncate(0)
-var.write(asd)
-var.close
-
