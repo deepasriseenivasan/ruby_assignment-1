662469e78402b3f98bd30b8edb662860818e4a2c
deepasriseenivasan <deepasrideeps1997@gmail.com>
2021-10-19 13:13:55 +0530

    if &t ifelse

---
+++ if_if_else/if.rb
@@ -0,0 +1,20 @@
+a=gets.chomp.to_i
+b=gets.chomp.to_i
+
+if a>b
+    puts "a is bigger"
+end
+if b>a
+    puts"b is bigger"
+end
+if a==b
+    puts"both r eql"
+end
+c=gets.chomp
+d=gets.chomp
+if c==d
+    puts"both r same string"
+end
+if c!=d
+    puts"not eql"
+end
---
+++ if_if_else/ifelse.rb
@@ -0,0 +1,17 @@
+a=gets.chomp.to_i
+b=gets.chomp.to_i
+
+if a>b
+    puts "a is bigger"
+elsif a<b
+    puts"b is bigger"
+else 
+    puts"both r equal"
+end
+c=gets.chomp
+d=gets.chomp
+if c==d
+    puts "both r equal"
+else
+    puts"both r not equal"
+end
